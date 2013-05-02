af5ba36bbea206751f063904399bc99f8a579168
lonnen <EMAIL>
2013-05-02 15:00:59 -0700

    lonnen's dotfiles

--- Puppetfile
+++ Puppetfile
@@ -34,4 +34,5 @@ github "sudo",     "1.0.0"
 # Optional/custom modules. There are tons available at
 # https://github.com/boxen.
 
+github "dropbox", "1.1.0"
 github "sublime_text_2", "1.1.0"
--- modules/people/manifests/lonnen.pp
+++ modules/people/manifests/lonnen.pp
@@ -1,10 +1,16 @@
 class people::lonnen {
+  include dropbox
   include sublime_text_2
 
   $home = "/Users/${::boxen_user}"
   $repos = "${home}/repos"
 
-  repository { "${repos}/dotfiles":
-    source => 'lonnen/dotfiles',
+  repository { "${repos}/socorro":
+    source => 'lonnen/socorro',
+    alias => 'clone-socorro'
+  }
+
+  exec { 'bash -c "$(curl -fsSL https://raw.github.com/lonnen/dotfiles/master/bin/dotfiles)" && source ~/.bashrc':
+    alias => 'install-dotfiles'
   }
 }
