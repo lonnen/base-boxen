424636b9e31d79e1c20cea6e3d8070df1fc3fd02
Matt Basta <me@mattbasta.com>
2013-07-16 14:36:48 -0700

    Update mattbasta.pp

--- modules/people/manifests/mattbasta.pp
+++ modules/people/manifests/mattbasta.pp
@@ -1,17 +1,32 @@
 class people::mattbasta {
-  include alfred
   include cloudapp
   include dropbox
   include limechat
   include minecraft
   include skype
   include spotify
+  #include vidyo
 
-  $home = "/Users/${::luser}"
-  $repos = "/opt"
+  $home     = "/Users/${::luser}"
+  $repos    = "/opt"
 
-  repository { "${repos}/fireplace":
-    source => 'mattbasta/fireplace',
-    alias => 'clone-fireplace'
+  include projects::fireplace
+  include projects::amo-validator
+  include projects::app-validator
+  
+  git::config::global { 'user.name':
+    value => 'Matt Basta'
   }
+
+  git::config::global { 'user.email':
+    value => 'me@mattbasta.com'
+  }
+
+  repository { "${repos}/hugbot":
+    source => 'mattbasta/hugbot',
+    alias => 'clone-hugbot'
+  }
+
+  class { 'nodejs::global': version => 'v0.10.5' }
+  nodejs::module { 'grunt-cli': node_version => 'v0.10.5' }
 }
