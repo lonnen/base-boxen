c627d05e3f7027e298ee762387d79dcb9f353e78
lonnen <chris.lonnen@gmail.com>
2014-02-25 19:53:44 -0800

    update lonnen's manifest with textual

--- modules/people/manifests/lonnen.pp
+++ modules/people/manifests/lonnen.pp
@@ -24,6 +24,7 @@ class people::lonnen {
   include spotify
   include sublime_text_3
   include sublime_text_2
+  include textual
   include transmission
   include viscosity
   include vmware_fusion
